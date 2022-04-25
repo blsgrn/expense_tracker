40c6ace9c4c7dba2f006f14a0d0096f2b4cf64fa
Subramaniam Balasegaran <blsgrn@tutanota.com>
2022-04-25 22:17:55 +0530

    add data array file in directory named data

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,12 +1,28 @@
+import Card from "../shared/Card";
 function Data() {
   const Date = "May 13, 2022";
   const Amount = 2000;
 
   return (
-    <div className="data">
-      <input type={"date"} value={Date} />
-      <input type={"number"} value={Amount} />
-    </div>
+    <>
+      <Card>
+        <div className="data">
+          <div className="date">
+            <form>
+              <label htmlFor="date">Date </label>
+              <input type="date" value={Date} />
+            </form>
+          </div>
+
+          <div className="amount">
+            <form>
+              <label htmlFor="amount">Amount </label>
+              <input type="amount" value={Amount} />
+            </form>
+          </div>
+        </div>
+      </Card>
+    </>
   );
 }
 
---
+++ src/data/EntryData.js
@@ -0,0 +1,7 @@
+const EntryData = [
+  { id: 11, date: "13.05.1978", amt: 200 },
+  { id: 33, date: "08.04.1980", amt: 2000 },
+  { id: 88, date: "18.07.1956", amt: 500 },
+];
+
+export default EntryData;
